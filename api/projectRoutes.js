01d3946e73d0971745427225186858dc2643a28a
decagondev <decagondevelopment@gmail.com>
2018-10-05 17:47:43 +0100

    (MVP) : projectRoutes.js : import project model, add get projects endpoint

--- api/projectRoutes.js
+++ api/projectRoutes.js
@@ -3,5 +3,17 @@ const express = require("express");
 // setup express router
 const router = express.Router();
 
+// import project model
+const projects = require("../data/helpers/projectModel");
+
+// get projects endpoint
+router.get("/", async (req, res) => {
+  try {
+    const allProjects = await projects.get();
+    res.status(200).json(allProjects);
+  } catch (error) {
+    res.status(500).json({ message: "projects could not be retrieved" });
+  }
+});
 // export router
 module.exports = router;
