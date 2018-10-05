f34e9b4d5d5ff60e8b35cb451139920150b63ac8
decagondev <decagondevelopment@gmail.com>
2018-10-05 17:54:33 +0100

    (MVP) : projectRoutes.js : import project model, add put project endpoint (edit specific project based upon project id)

--- api/projectRoutes.js
+++ api/projectRoutes.js
@@ -62,5 +62,31 @@ router.post("/", async (req, res) => {
   }
 });
 
+// put project endpoint (edit specific project based upon project id)
+router.put("/:id", async (req, res) => {
+  try {
+    // no name check
+    if (!req.body.name) {
+      return res.status(400).json({ message: "enter a name" });
+    }
+
+    // no description check
+    if (!req.body.description) {
+      return res.status(400).json({ message: "enter a description" });
+    }
+
+    // name length check
+    if (req.body.name.length > 128) {
+      return res
+        .status(400)
+        .json({ message: "name must be less than 128 characters" });
+    }
+    const project = await projects.update(req.params.id, req.body);
+    res.status(200).json(project);
+  } catch (error) {
+    res.status(500).json({ message: "project could not be saved" });
+  }
+});
+
 // export router
 module.exports = router;
