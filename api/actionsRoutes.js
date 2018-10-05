8c76c77d72a5cf3bbbf9f724e2b32193f97ccb03
decagondev <decagondevelopment@gmail.com>
2018-10-05 17:08:50 +0100

    (MVP) : actionRoutes.js : import the actions model

--- api/actionsRoutes.js
+++ api/actionsRoutes.js
@@ -3,5 +3,8 @@ const express = require("express");
 // setup express router
 const router = express.Router();
 
+// import the actions model
+const actions = require("../data/helpers/actionModel");
+
 // export router
 module.exports = router;
