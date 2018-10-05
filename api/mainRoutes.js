1b43f75583ff176f6ac33887ebda7b4da4872956
decagondev <decagondevelopment@gmail.com>
2018-10-05 17:45:10 +0100

    (MVP) : mainRoutes.js : import actionRoutes, add actions route to the router

--- api/mainRoutes.js
+++ api/mainRoutes.js
@@ -2,3 +2,9 @@ const express = require("express");
 
 // setup express reouter
 const router = express.Router();
+
+// import the action routes
+const actionRoutes = require("./actionsRoutes");
+
+// add a route to the action routes
+router.add("/actions", actionRoutes);
