0a22c474ddaee1a58eb3e2b685aa30edbebc7138
decagondev <decagondevelopment@gmail.com>
2018-10-05 17:10:17 +0100

    (MVP) : actionRoutes.js : add the get actions endpoint route

--- api/actionsRoutes.js
+++ api/actionsRoutes.js
@@ -6,5 +6,15 @@ const router = express.Router();
 // import the actions model
 const actions = require("../data/helpers/actionModel");
 
+// get actions endpoint
+router.get("/", async (req, res) => {
+  try {
+    const allActions = await actions.get();
+    res.status(200).json(allActions);
+  } catch (error) {
+    res.status(500).json({ message: "actions could not be retrieved." });
+  }
+});
+
 // export router
 module.exports = router;
